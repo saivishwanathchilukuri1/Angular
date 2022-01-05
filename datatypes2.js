58bb2801c85359b39696530cfc8639acff15edeb
sai vishwanath Chilukuri <saivishwanath11596@gmail.com>
2022-01-05 19:17:29 -0400

    first commit

---
+++ datatypes2.js
@@ -0,0 +1,10 @@
+function F2() {
+    var name = "Sai";
+    var val = 10;
+    if (val < 11) {
+        var name_1 = "Vishu";
+        console.log('Inside the function' + name_1);
+    }
+    console.log('Outside the function' + name);
+}
+F2();
---
+++ datatypes2.ts
@@ -0,0 +1,12 @@
+function F2()
+{
+    let name:string="Sai";
+    let  val:number=10;
+    if(val<11)
+    {
+        let name:string="Vishu";
+        console.log('Inside the function'+ name);
+    }
+    console.log('Outside the function'+name);
+}
+F2();
